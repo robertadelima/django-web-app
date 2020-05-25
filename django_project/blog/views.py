7b7fa56e8122df03514217942fa7c140b5063561
robertadelima <robertarl@gmail.com>
2020-05-25 16:30:33 -0300

    testing post in templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Roberta',
+        'title': 'First Post',
+        'content': 'Content for my first post',
+        'date_posted': 'November 10, 2016'
+    },
+        {
+        'author': 'Lima',
+        'title': 'About Monday',
+        'content': 'Content for my post regarding mondays',
+        'date_posted': 'May 25s, 2020'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    #return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html', context)
+    
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    #return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
